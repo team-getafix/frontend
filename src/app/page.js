15f62a1698aeae3c85f8496ad1a4ea330197f1b4
Kristian Petrov <krisi.petroff@gmail.com>
2025-02-12 21:35:07 +0200

    SignUp

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,19 @@
-import Form from "@/components/Form";
+"use client";
+
+import { useState } from 'react';
+import SignIn from "@/components/SignIn";
+import SignUp from "@/components/SignUp";
 
 function App() {
+  const [isSignUp, setIsSignUp] = useState(false);
+
   return (
-    <div className="flex w-full h-screen">
-      <div className="w-full flex items-center justify-center lg:w-1/2">
-        <Form />
+    <div className="flex w-full h-screen overflow-hidden">
+      <div className={`w-full flex items-center justify-center lg:w-1/2 lg:transition-transform lg:duration-500 ${isSignUp ? 'lg:translate-x-full' : ''}`}>
+        {isSignUp ? <SignUp setIsSignUp={setIsSignUp} /> : <SignIn setIsSignUp={setIsSignUp} />}
       </div>
-      <div className="hidden relative lg:flex h-full w-1/2 items-center justify-center bg-gray-200">
-        <div className="w-60 h-60 bg-gradient-to-tr from-violet-500 to-pink-500 rounded-full animate-spin"/>
+      <div id="sphere" className={`hidden relative lg:flex h-full w-1/2 items-center justify-center bg-gray-200 lg:transition-transform lg:duration-500 ${isSignUp ? 'lg:-translate-x-full' : ''}`}>
+        <div className="w-60 h-60 bg-gradient-to-tr from-orange-500 to-yellow-500 rounded-full animate-spin"/>
         <div className="w-full h-1/2 absolute bottom-0 bg-white/10 backdrop-blur-lg"/>
       </div>
     </div>
---
+++ src/components/SignUp.js
@@ -0,0 +1,29 @@
+export default function SignUp({ setIsSignUp }) {
+    return (
+        <div className="bg-white duration-300 px-10 py-20 rounded-2xl border-2 border-gray-200">
+            <h1 className="text-5xl font-semibold"> Create Account</h1>
+            <p className="font-medium text-lg text-gray-500 mt-4">Please fill in the details to create an account.</p>
+            <div className="mt-8">
+                <div>
+                    <label className="text-lg font-medium">Email</label>
+                    <input className="w-full border-2 border-gray-100 rounded-xl p-4 mt-1 bg-transparent" placeholder="getafix@example.com"></input>
+                </div>
+                <div>
+                    <label className="text-lg font-medium">Password</label>
+                    <input className="w-full border-2 border-gray-100 rounded-xl p-4 mt-1 bg-transparent" placeholder="Enter your password" type='password'></input>
+                </div>
+                <div>
+                    <label className="text-lg font-medium">Confirm Password</label>
+                    <input className="w-full border-2 border-gray-100 rounded-xl p-4 mt-1 bg-transparent" placeholder="Confirm your password" type='password'></input>
+                </div>
+                <div className="mt-8 flex flex-col gap-y-4">
+                    <button className="active:scale-[.98] active:duration-75 ease-in-out hover:scale-[1.01] transition-all bg-orange-500 text-white text-lg rounded-xl py-3 font-bold">Sign Up</button>
+                </div>
+                <div className="mt-8 flex justify-center items-center">
+                    <p className="font-medium text-base">Already have an account?</p>
+                    <button className="text-orange-500 text-base font-medium ml-2" onClick={() => setIsSignUp(false)}>Sign in</button>
+                </div>
+            </div>
+        </div>
+    )
+}
