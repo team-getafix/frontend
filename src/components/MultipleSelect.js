f9341ce2cfe573301b2e729e78f6efc5ecf02a22
Ba6taTi <krasizh07@gmail.com>
2025-02-22 22:07:49 +0200

    fix: removed title from multiple select

--- src/components/MultipleSelect.js
+++ src/components/MultipleSelect.js
@@ -1,10 +1,9 @@
 import { addScaleCorrector } from "framer-motion";
 import React, { useState } from "react";
 
-const MultipleSelect = ({title, options}) => {
+const MultipleSelect = ({options}) => {
     /** 
      * Custom select that supports multiple selection
-     * @param {title} the title of the select
      * @param {options} options must be a list filled with dictionaries (each one a different option) that have the following keys: value and label
     */
 
@@ -20,16 +19,14 @@ const MultipleSelect = ({title, options}) => {
         }
     };
 
-    return (
+    // TODO: make the checks black to match the ui
+    return ( 
         <div className="p-2 bg-white rounded shadow">
-            <h2 className="text-xl font-semibold text-gray-800 mb-4">
-                { title }
-            </h2>
             <div className="space-y-3">
                 {options.map((option) => (
-                    <label key={option.value} className={`flex items-center p-3 rounded-lg transition-all duration-200 ${selectedOptions.includes(option.value) ? "bg-blue-50 border border-blue-200" : "bg-gray-50 hover:bg-gray-100 border border-gray-200"}`}>
-                        <input type="checkbox" value={option.value} checked={selectedOptions.includes(option.value)} onChange={handleCheckboxChange} className="w-5 h-5 text-blue-600 border-gray-300 rounded focus:ring-blue-500"/>
-                        <span className="ml-3 text-sm text-gray-700">{option.label}</span>
+                    <label key={option.value} className={`flex items-center p-3 rounded-lg transition-all duration-200 ${selectedOptions.includes(option.value) ? "bg-gray-50 border border-gray-200" : "bg-gray-50 hover:bg-gray-100 border border-gray-200"}`}>
+                        <input type="checkbox" value={option.value} checked={selectedOptions.includes(option.value)} onChange={handleCheckboxChange} className="w-5 h-5 text-gray-400 border-gray-300 rounded focus:ring-gray-950"/>
+                        <span className="ml-3 text-lg text-gray-700">{option.label}</span>
                     </label>
                 ))}
             </div>
