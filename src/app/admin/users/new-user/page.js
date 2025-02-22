b8947ff582fb159780061debdf5ef7f1387f7795
Ba6taTi <krasizh07@gmail.com>
2025-02-22 14:51:23 +0200

    feat: user creation page

---
+++ src/app/admin/users/new-user/page.js
@@ -0,0 +1,14 @@
+"use client";
+
+import NewUser from '@/components/NewUser';
+import React, { useState } from 'react';
+
+// TODO: handle 409, 200, ???
+
+function CreateUserView() {
+    return ( // TODO: return 404
+        <NewUser/>
+    )
+}
+
+export default CreateUserView
--- src/components/NewUser.js
+++ src/components/NewUser.js
@@ -1,6 +1,15 @@
+import { capitalize } from "@/utils/stringUtils";
+import { useState } from "react";
+
 export default function NewUser() {
+
     function createUser(formData) {
-        localStorage.setItem('token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjEiLCJyb2xlIjoiYWRtaW4iLCJpYXQiOjE3NDAxNTkzMzQsImV4cCI6MTc0MDE2MjkzNH0.UcGuStFBDtiI0tbJBnERv-OJiZJkFHv4YJiBpbiDD5s');
+        localStorage.setItem('token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjEiLCJyb2xlIjoiYWRtaW4iLCJpYXQiOjE3NDAyMjYxNjYsImV4cCI6MTc0MDIyOTc2Nn0.VaJ3_h4GKDIxpKzA65bMhJsAPxoBa9ZuCZCXd2g_hcI');
+
+        const email = formData.get('email');
+        const firstName = formData.get('firstName');
+        const lastName = formData.get('lastName');
+        const role = formData.get('role');
 
         const response = fetch('http://localhost:4000/api/auth/register', {
             method: 'POST',
@@ -9,19 +18,20 @@ export default function NewUser() {
                 'Authorization': `Bearer ${localStorage.getItem('token')}`
             },
             body: JSON.stringify({
-                email: formData.get('email'),
-                firstName: formData.get('firstName'),
-                lastName: formData.get('lastName'),
-                role: formData.get('role')
+                email: email,
+                firstName: firstName,
+                lastName: lastName,
+                role: role
 
             }),
         }).then(response => {
             if (response.ok) {
-                alert(`${formData.get('role')} ${formData.get('firstName')} ${formData.get('lastName')} created successfully`);
-            } else { // TODO: not clear message
-                const errorData = response.status;
-                console.error(errorData);
-                alert(errorData);
+                alert(`${capitalize(formData.get('role'))} ${formData.get('firstName')} ${formData.get('lastName')} created successfully`);
+            } else if (response.status == 409) {
+                alert(`User with email "${formData.get('email')}" already exists`);
+            } else {
+                console.error(`${response}`);
+                alert(`${response.status}: ${response.statusText}`);
             }
         }).catch(error => {
             console.error(error);
@@ -29,48 +39,53 @@ export default function NewUser() {
         });
     }
 
-    return (
-        <form action={createUser}>
-                <div className="mb-4">
-                    <label>
-                        Email
-                    </label>
-                    <input id="email" name="email" className="shadow border rounded w-full pl-2" placeholder="example@example.com" ></input>
-                </div>
-                {/* <div className="mb-4">
-                    <label>
-                        Password
-                    </label>
-                    <input id="password" name="password" className="shadow border rounded w-full" placeholder="********" type="password"></input>
-                </div> */}
+    return (    
+        <>
+            <div className="flex items-center justify-center w-screen h-screen" suppressHydrationWarning>
+                <form action={createUser}>
+                    <div className="mb-4">
+                        <label>
+                            Email
+                        </label>
+                        <input id="email" name="email" className="shadow border rounded w-full pl-2" placeholder="example@example.com" ></input>
+                    </div>
+                    {/* <div className="mb-4">
+                        <label>
+                            Password
+                        </label>
+                        <input id="password" name="password" className="shadow border rounded w-full" placeholder="********" type="password"></input>
+                    </div> */}
 
-                <div className="mb-4">
-                    <label>
-                        First Name
-                    </label>
-                    <input id="firstName" name="firstName" className="shadow border rounded w-full pl-2" placeholder="John"></input>
-                </div>
-                <div className="mb-4">
-                    <label>
-                        Last Name
-                    </label>
-                    <input id="lastName" name="lastName" className="shadow border rounded w-full pl-2" placeholder="Doe"></input>
-                </div>
-                <div className="mb-4">
-                    <label>
-                        Role
-                    </label>
-                    <select id="role" name="role" className="shadow border rounded w-full pl-1">
-                        <option value="student" defaultValue>Student</option>
-                        <option value="teacher">Teacher</option>
-                        <option value="admin">Admin</option>
-                    </select>
-                </div>
-                <div className="mb-4">
-                    <button className="shadow border rounded-full bg-blue-500 text-white px-4 py-1" type="submit">
-                        Create
-                    </button>
-                </div>
-            </form>
+                    <div className="mb-4">
+                        <label>
+                            First Name
+                        </label>
+                        <input id="firstName" name="firstName" className="shadow border rounded w-full pl-2" placeholder="John"></input>
+                    </div>
+                    <div className="mb-4">
+                        <label>
+                            Last Name
+                        </label>
+                        <input id="lastName" name="lastName" className="shadow border rounded w-full pl-2" placeholder="Doe"></input>
+                    </div>
+                    <div className="mb-4">
+                        <label>
+                            Role
+                        </label>
+                        <select id="role" name="role" className="shadow border rounded w-full pl-1">
+                            <option value="student" defaultValue>Student</option>
+                            <option value="teacher">Teacher</option>
+                            <option value="admin">Admin</option>
+                        </select>
+                    </div>
+                    <div className="mb-4">
+                        <button className="shadow border rounded-full bg-blue-500 text-white px-4 py-1" type="submit">
+                            Create
+                        </button>
+                    </div>
+                </form>
+            </div>
+
+        </>
     )
 }
