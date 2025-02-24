7dc7145d2fff02d0e1a3e2df128f47079be9d061
Ba6taTi <krasizh07@gmail.com>
2025-02-24 18:27:07 +0200

    fix: added new param name to be accessible by the form

--- src/components/MultipleSelect.js
+++ src/components/MultipleSelect.js
@@ -1,9 +1,10 @@
 import { addScaleCorrector } from "framer-motion";
 import React, { useState } from "react";
 
-const MultipleSelect = ({options}) => {
+const MultipleSelect = ({name, options}) => {
     /** 
      * Custom select that supports multiple selection
+     * @param {name} name that will be referenced in form
      * @param {options} options must be a list filled with dictionaries (each one a different option) that have the following keys: value and label
     */
 
@@ -25,7 +26,7 @@ const MultipleSelect = ({options}) => {
             <div className="space-y-3">
                 {options.map((option) => (
                     <label key={option.value} className={`flex items-center p-3 rounded-lg transition-all duration-200 ${selectedOptions.includes(option.value) ? "bg-gray-50 border border-gray-200" : "bg-gray-50 hover:bg-gray-100 border border-gray-200"}`}>
-                        <input type="checkbox" value={option.value} checked={selectedOptions.includes(option.value)} onChange={handleCheckboxChange} className="w-5 h-5 text-gray-400 border-gray-300 rounded focus:ring-gray-950"/>
+                        <input name={name} type="checkbox" value={option.value} checked={selectedOptions.includes(option.value)} onChange={handleCheckboxChange} className="w-5 h-5 text-gray-400 border-gray-300 rounded focus:ring-gray-950"/>
                         <span className="ml-3 text-lg text-gray-700">{option.label}</span>
                     </label>
                 ))}
