1081d71c211f6c4323136db459b77b1dd4a3a836
Ba6taTi <krasizh07@gmail.com>
2025-02-25 17:45:41 +0200

    feat: admin page (base)

--- src/app/admin/page.js
+++ src/app/admin/page.js
@@ -1,5 +1,23 @@
 export default function AdminPage() {
     return (
-        <h1>Admin Page</h1>
+        <div className="flex justify-center items-center m-10 w-full h-full">
+            <div className="grid grid-cols-2 gap-4 justify-center text-xl">
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold rounded px-4 py-5">
+                    New Class
+                </button>
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold rounded px-4 py-5">
+                    View Classes
+                </button>
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold rounded px-4 py-5">
+                    New Subject
+                </button>
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold rounded px-4 py-5">
+                    View Subjects
+                </button>
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold rounded px-4 py-5">
+                    New User
+                </button>
+            </div>
+        </div>
     )
 }
