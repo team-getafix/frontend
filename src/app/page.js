cddf25cce3aec6b46fa5c605618d8a0cfcaba28f
Ba6taTi <krasizh07@gmail.com>
2025-02-26 22:21:33 +0200

    fix: fetching  class only for students and teachers

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,46 @@
 "use client";
 
 import React, { useState, useEffect } from 'react';
-import NoClasses from '@/components/NoClasses';
+import CourseJoin from "@/components/CourseJoin";
+import CourseCreate from "@/components/CourseCreate";
+import ClassCard from "@/components/ClassCard";
+import { isTokenValid } from '@/utils/authUtils';
 
-export default function App() {
+function App() {
+  const [isClient, setIsClient] = useState(false);
+  const [showCourseJoin, setShowCourseJoin] = useState(false);
+  const [showCourseCreate, setShowCourseCreate] = useState(false);
+
+  const handleJoinCourseClick = () => {
+    setShowCourseJoin(true);
+  };
+
+  const handleCloseCourseJoin = () => {
+    setShowCourseJoin(false);
+  };
+
+  const handleCreateCourseClick = () => {
+    setShowCourseCreate(true);
+  };
+
+  const handleCloseCourseCreate = () => {
+    setShowCourseCreate(false);
+  };
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  if (!isClient) {
+    return;
+  }
+
+  if (isTokenValid() && !isAdmin()) {
+    return (
+      <ClassCard/>
+    )
+  }
+
   return (
     <div className="m-10 w-full">
       <NoClasses/>
