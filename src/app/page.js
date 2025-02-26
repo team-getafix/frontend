445d6885fcd21aea83177908fd81aebc15f0ec2c
Kristian Petrov <krisi.petroff@gmail.com>
2025-02-26 23:14:18 +0200

    fix: update authentication handling and remove NoClasses component

--- src/app/page.js
+++ src/app/page.js
@@ -4,8 +4,7 @@ import React, { useState, useEffect } from 'react';
 import CourseJoin from "@/components/CourseJoin";
 import CourseCreate from "@/components/CourseCreate";
 import ClassCard from "@/components/ClassCard";
-import { isTokenValid, isAdmin } from '@/utils/authUtils';
-import NoClasses from '@/components/NoClasses';
+import { isTokenValid, isAdmin, isStudent, isTeacher } from '@/utils/authUtils';
 
 export default function App() {
   const [isClient, setIsClient] = useState(false);
@@ -36,15 +35,45 @@ export default function App() {
     return;
   }
 
-  if (isTokenValid() && !isAdmin()) {
-    return (
-      <ClassCard/>
-    )
+  // if (isTokenValid() && !isAdmin()) {
+  //   return (
+  //     <ClassCard/>
+  //   )
+  // }
+
+  if (isTokenValid() && isAdmin()) {
+    if (typeof window !== 'undefined') {
+      window.location.href = '/admin';
+    }
+    return null;
+  } else if (!isTokenValid()) {
+    if (typeof window !== 'undefined') {
+      window.location.href = '/login';
+    }
+    return null;
   }
 
   return (
     <div className="m-10 w-full">
-      <NoClasses/>
+      <div id='no-classes'>
+        <div className="flex flex-col items-end justify-end">
+          <img src="https://www.gstatic.com/classroom/web/home/dark_create_class_arrow.svg" alt="Vercel Logo" className="h-12 w-12 -translate-x-3 -translate-y-3 scale-150" />
+          <h1 className="text-center font-medium">Don’t see your classes?<br/>Try another account</h1>
+        </div>
+        <div className="flex flex-col text-center items-center justify-center h-full m-5 font-semibold">
+          <h1>Add a course to get started</h1>
+          <div className="justify-between sm:space-x-4 mt-2">
+            {isStudent() && isTokenValid() && (
+              <h2>You are a student</h2>
+            )}
+            {isTeacher() && isTokenValid() && (
+              <h2>You are a teacher</h2>
+            )}
+          </div>
+        </div>
+        {showCourseJoin && <CourseJoin close={handleCloseCourseJoin} />}
+        {showCourseCreate && <CourseCreate close={handleCloseCourseCreate} />}
+      </div>
     </div>
   );
 }
